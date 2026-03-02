6710b9832f2638b1a6d6ee97e01ab9d3bf95d292
LUKE SMILEY <227441@andrews.esc18.net>
2026-03-02 11:10:47 -0600

    Started 3rd assignment

--- src/main/java/StringRipper.java
+++ src/main/java/StringRipper.java
@@ -1,10 +1,10 @@
 //(c) A+ Computer Science
 //www.apluscompsci.com
 
-//Name -
-//Date -
-//Class -
-//Lab  -
+//Name - Luke Smiley
+//Date - 3/2/26
+//Class - Comp Sci 1
+//Lab  - String Ripper
 
 import static java.lang.System.*;
 
@@ -14,6 +14,7 @@ public class StringRipper
 	
 	public StringRipper()
 	{
+		setString();
 	}
 
 	public StringRipper(String s)
@@ -22,15 +23,18 @@ public class StringRipper
 	
    public void setString(String s)
    {
+	word=s;
    }	
 
 	public String ripString(int x, int y)
 	{
+		word.subString(x, y);
 		return "";
 	}
 
  	public String toString()
  	{
+		return word;
  		return "\n\n";
 	}
 }
